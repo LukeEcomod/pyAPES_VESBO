c08d44ad75ca3cc0a3bc68156e464ad6dd9a6c70
Antti-Jussi Kieloaho <antti-jussi.kieloaho@luke.fi>
2018-10-19 13:57:32 +0300

    Starting to implement group of simulations

---
+++ parallelAPES.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 11:07:09 2018
+
+@author: ajkieloaho
+"""
+
+from multiprocess import Process
+
+class Worker(Process):
+    """ Multiproces
+    """
+
+
+
+
+
+if __name__ == '__main__':
+    raise NotImplementedError
+else:
+    print('parallelAPES imported not executed')
+# EOF
