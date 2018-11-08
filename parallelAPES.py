fcf641f0545d6fb3521bca571699081e908e3d6d
Antti-Jussi Kieloaho <antti-jussi.kieloaho@luke.fi>
2018-11-08 10:04:38 +0200

    comandline interface added

--- parallelAPES.py
+++ parallelAPES.py
@@ -3,27 +3,38 @@
 """
 Created on Fri Oct 19 11:07:09 2018
 
+TODO:
+    - dump parameter space to file
+    - check if filter/adapeter can be used for configure loggers Formatter
+    (There is need for add nsim, process id can be added somwhere directily)
+
+
 @author: ajkieloaho
 """
 
 import os
-from multiprocessing import Process, JoinableQueue, Queue, cpu_count
-import multiprocessing
+from multiprocessing import Process, Queue  # , cpu_count
+from psutil import cpu_count
 from copy import deepcopy
-from pyAPES import initialize_netcdf, _write_ncf
+
+from tools.iotools import initialize_netcdf, write_ncf
+from tools.iotools import jsonify
 from pyAPES import Model
 
 import time
 
 import logging
 import logging.handlers
-import Threading
+import logging.config
+import threading
 
 
 def _result_writer(queue, ncf_param):
     """
+    Args:
+        queue (Queue): results queue
+        ncf_param: parameters to initialize NetCDF4 file
     """
-
     ncf, _ = initialize_netcdf(
         variables=ncf_param['variables'],
         sim=ncf_param['Nsim'],
@@ -33,52 +44,57 @@ def _result_writer(queue, ncf_param):
         forcing=ncf_param['forcing'],
         filename=ncf_param['file_name'])
 
-    print(" Queue consumer is listening")
-
-    flag = True
-    while flag:
-
+    while True:
+        # results is tuple (Nsim, data)
         results = queue.get()
 
-        if results['Nsim'] == -999:
-            print('Writing of results is DONE!')
+        if results is None:
             ncf.close()
-            queue.task_done()
-            flag = False
-        else:
-            print(' Processing simulation number: {}'.format(results['Nsim']))
-            _write_ncf(nsim=results['Nsim'], results=results['data'], ncf=ncf)
-            queue.task_done()
+            break
+
+        write_ncf(nsim=results[0], results=results[1], ncf=ncf)
+
 
 # logging to a single file from multiple processes
 # https://docs.python.org/dev/howto/logging-cookbook.html#logging-to-a-single-file-from-multiple-processes
 
 
-def logger_thread(queue):
+def _logger_listener(queue):
+    """
+    Args:
+        queue (Queue): logging queue
+    """
+
     while True:
         record = queue.get()
+
         if record is None:
             break
+
         logger = logging.getLogger(record.name)
         logger.handle(record)
 
 
 def _worker(task_queue, result_queue, logging_queue):
     """
+    Args:
+        task_queue (Queue): queue of task initializing parameters
+        result_queue (Queue): queue of model calculation results
+        logging_queue (Queue): queue for model loggers
     """
-    # setting up logging
-    # might be worth of doing inside while loop
+
+    # --- LOGGING ---
     qh = logging.handlers.QueueHandler(logging_queue)
     root = logging.getLogger()
 
     # !!! root level set should be in configuration dictionary!!!
-
+    root.handlers = []
     root.setLevel(logging.DEBUG)
     root.addHandler(qh)
-
-    # task queue sentinel
+    # --- TASK QUEUE LISTENER ---
     while True:
         task = task_queue.get()
+
         if task is None:
             break
 
@@ -95,29 +111,44 @@ def _worker(task_queue, result_queue, logging_queue):
         # can return something if everything went right
 
 
-def driver(create_ncf=True, dbhfile='letto2014.txt', logging_configuration=None):
+def driver(create_ncf=True,
+           dbhfile='letto2014.txt',
+           logging_configuration=None,
+           Ncpu=None):
     """
+    Args:
+        create_ncf (boolean)
+        dbhfile (str)
+        logging_configuration (dict)
+        ncpu (int)
     """
-    from parameters.canopy import get_cpara
-    from parameters.sensitivity import parameters, iterate_parameters
     from parameters.general import gpara
-    from parameters.soil import spara
+    from parameters.canopy import get_cpara
+    from parameters.soil import get_spara
+
+    from parameters.sensitivity import get_parameters, iterate_parameters
+
     from tools.iotools import read_forcing
 
+    # from logging.config import dictConfig
     # --- LOGGING ---
 
     logging_queue = Queue()
 
     logging.config.dictConfig(logging_configuration)
-    logging_thread = Threading.Thread(target=logger_thread, args=(logging_queue))
+    logging_thread = threading.Thread(target=_logger_listener,
+                                      args=(logging_queue,))
     logging_thread.start()
 
-    # --- TASKS ---
-    # Refactor: parameter handling in separate function
+    logger = logging.getLogger()
 
+    # --- PARAMETERS ---
+    # Refactor: parameter handling in separate function
+    parameters = get_parameters(name=None)
     Nsim = parameters['count']
 
     cpara = get_cpara(dbhfile=dbhfile)
+    spara = get_spara('organic')
 
     default_para = {
             'canopy': cpara,
@@ -125,26 +156,32 @@ def driver(create_ncf=True, dbhfile='letto2014.txt', logging_configuration=None)
             'general': gpara
             }
 
-    para_space = [iterate_parameters(parameters, deepcopy(default_para), count) for count in range(Nsim)]
+    para_space = [iterate_parameters(
+        parameters,
+        deepcopy(default_para),
+        count) for count in range(Nsim)]
 
+    timestr = time.strftime('%Y%m%d%H%M')
+
+    # save parameters into json file
+    jsonify(para_space, file_name='results/'+timestr+'_pyAPES_parameters.json')
+
+    # --- FORCING ---
     forcing = read_forcing(gpara['forc_filename'],
                            gpara['start_time'],
                            gpara['end_time'],
                            dt=gpara['dt'])
 
-
-    model_input = {'forcing': forcing}
-
+    # --- TASKS ---
     tasks = []
-    for k in range(Nsim):
-        model_input.update({'nsim': k})
-        model_input.update(para_space[k])
-        tasks.append(deepcopy(model_input))
+    for k, para in enumerate(para_space):
+        para.update({'nsim': k,
+                     'forcing': forcing})
+        tasks.append(deepcopy(para))
 
     # --- NETCDF4 ---
-    results_queue = JoinableQueue()
+    writing_queue = Queue()
 
-    timestr = time.strftime('%Y%m%d%H%M')
     filename = timestr + '_pyAPES_results.nc'
 
     pyAPES_folder = os.getcwd()
@@ -160,106 +197,74 @@ def driver(create_ncf=True, dbhfile='letto2014.txt', logging_configuration=None)
                  'output_path': filepath,
                  }
 
-
-    writing_process = Process(target=_result_writer, args=(results_queue, ncf_param))
-
-    writing_process.deamon = True
-    writing_process.start()
+    writing_thread = threading.Thread(target=_result_writer,
+                                      args=(writing_queue, ncf_param,))
+    writing_thread.start()
 
     # --- PROCESSES ---
     task_queue = Queue()
     for para in para_space:
         task_queue.put(para)
 
-    N_workers = cpu_count() - 2
+    if Ncpu is None:
+        Ncpu = cpu_count(logical=False)
+
+    if Nsim > (Ncpu - 1):
+        N_workers = Ncpu - 1
+    else:
+        N_workers = Nsim
+
+    logger.info('Number of worker processes is {}, number of simulations: {}'.format(N_workers, Nsim))
+
+    running_time = time.time()
 
     workers = []
     for k in range(N_workers):
-        workers.append(Process(target=_worker, args=(task_queue, results_queue, logging_queue)))
+        workers.append(Process(target=_worker,
+                               args=(task_queue,
+                                     writing_queue,
+                                     logging_queue,
+                                     )))
         task_queue.put(None)
         workers[k].start()
 
-#    #--- RUNS IN MAIN PROCESS and WRITE IN ANOTHER ---
-#    for task in tasks:
-#
-#        model = Model(task['general'],
-#                      task['canopy'],
-#                      task['soil'],
-#                      task['forcing'],
-#                      task['nsim'])
-#        result = model.run()
-#        results_queue.put({'Nsim': task['nsim'], 'data': deepcopy(result)})
-#
-#        del result
-
     # --- CLOSE ---
 
-    # end worker processes
+    # join worker processes
     for w in workers:
         w.join()
 
-    # end writing queue
-    results_queue.put({'Nsim': -999, 'data': 'DONE'})
-    results_queue.join()
-
-    # end logging queue
+    # end logging queue and join
     logging_queue.put(None)
     logging_thread.join()
 
+    # end writing queue and join
+    writing_queue.put(None)
+    writing_thread.join()
+
+    logger.info('Running time %.2f seconds' % (time.time() - running_time))
+    logger.info('Results are in path: ' + ncf_param['output_path'])
 
     return ncf_param['output_path']
 
 
 if __name__ == '__main__':
-    import logging.config
+    import argparse
+    from parameters.general import parallel_logging_configuration
 
-    logging_configuration = {
-        'version': 1,
-#        'disable_existing_loggers': False,
-        'formatters': {
-                'default': {'format': '%(asctime)s %(levelname)s %(name)s %(message)s'},
-                'model': {'format': '%(levelname)s %(name)s %(funcName)s %(message)s'},
-                },
-        'handlers': {
-                'console': {
-                        'class' : 'logging.StreamHandler',
-                        'formatter': 'model',
-                        'level': 'INFO'  # CRITICAL, ERROR, WARNING, INFO, DEBUG
-                        },
-                'file': {
-                        'class': 'logging.FileHandler',
-                        'level': 'DEBUG',  # CRITICAL, ERROR, WARNING, INFO, DEBUG
-                        'formatter': 'model',
-                        'filename': 'pyAPES.log',
-                        'mode': 'w',  # a == append, w == overwrite
-                        },
-                },
-        'loggers': {
-                'pyAPES': {
-                        'handlers': ['file', 'console'],
-                        'level': 'INFO',  # CRITICAL, ERROR, WARNING, INFO, DEBUG
-#                        'propagate': True,
-                        },
-                'canopy':{
-                        'handlers': ['file', 'console'],
-                        'level': 'DEBUG',  # CRITICAL, ERROR, WARNING, INFO, DEBUG
-#                        'propagate': True,
-                        },
-                'soil':{
-                        'handlers': ['file', 'console'],
-                        'level': 'DEBUG',  # CRITICAL, ERROR, WARNING, INFO, DEBUG
-#                        'propagate': True,
-                        },
-                },
-        'root': {
-                'level': 'DEBUG',
-                'handlers': ['file', 'console']
-                }
-        }
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cpu', help='number of cpus to be used', type=int)
 
-    outputfile = driver(create_ncf=True,
-                        dbhfile="letto2014.txt",
-                        logging_configuration=logging_configuration)
+    args = parser.parse_args()
+
+    if args.cpu:
+        outputfile = driver(create_ncf=True,
+                            dbhfile='letto2014.txt',
+                            logging_configuration=parallel_logging_configuration,
+                            Ncpu=args.cpu)
+    else:
+        outputfile = driver(create_ncf=True,
+                            dbhfile="letto2014.txt",
+                            logging_configuration=parallel_logging_configuration)
 
     print(outputfile)
-
