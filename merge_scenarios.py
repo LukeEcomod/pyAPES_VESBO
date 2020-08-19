4f8f3c4937778cf234a7490556a0190ebb518cb5
Samuli Launiainen <samuli.launiainen@luke.fi>
2020-08-19 10:46:28 +0300

    new scenarios

--- merge_scenarios.py
+++ merge_scenarios.py
@@ -6,11 +6,12 @@ Created on Mon Aug 17 09:57:38 2020
 @author: 03081268
 """
 
-
+import pickle
 import xarray
 import glob
-import os
+#import os
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 # utility functions
@@ -32,83 +33,91 @@ files = glob.glob(fpath)
 
 data = []
 for k in range(1,len(files)):
+    print('reading ' + str(k))
     data.append(xarray.open_dataset(files[k]))
 print('done')
 
-#df = xarray.merge([xarray.open_dataset(f) for f in files]
-df = xarray.concat(data, 'date')
-df.to_netcdf(r'results/Scenarios/S1/S1all.nc', format='NETCDF4')
-print('ncf written')
+##df = xarray.merge([xarray.open_dataset(f) for f in files]
+#df = xarray.concat(data, 'date')
+#df.to_netcdf(r'results/Scenarios/S1/S1all.nc', format='NETCDF4')
+#print('ncf written')
 
 #%%
-d = xarray.open_dataset(r'results/Scenarios/S1/S1all.nc')
 
-dd = []
+out = []
 for k in range(0, sims):
-    dd.append(xarray_to_df(d, [
-                         'forcing_air_temperature',
-                         'forcing_precipitation',
-                         'forcing_pressure',
-                         'forcing_h2o',
-                         'forcing_co2',
-                         'forcing_wind_speed',
-                         'forcing_friction_velocity',
-                         'forcing_par',
-                         'forcing_nir',
-                         'forcing_lw_in',
-                         'canopy_LAI',
-                         #'canopy_energy_closure',
-                         #'canopy_fr_source',
-                         'canopy_SWnet',
-                         'canopy_LWnet',
-                         'canopy_Rnet',
-                         #'canopy_interception',
-                         #'canopy_interception_storage',
-                         'canopy_evaporation',
-                         #'canopy_condensation',
-                         #'canopy_condensation_drip',
-                         'canopy_throughfall',
-                         #'canopy_water_closure',
-                         'canopy_SH',
-                         'canopy_LE',
-                         'canopy_NEE',
-                         'canopy_GPP',
-                         'canopy_Reco',
-                         'canopy_transpiration',
-                         #'pt_total_gpp',
-                         #'pt_total_dark_respiration',
-                         #'pt_total_transpiration',
-                         #'pt_total_stomatal_conductance_h2o',
-                         #'pt_total_boundary_conductance_h2o',
-                         #'soil_temperature',
-                         #'soil_volumetric_water_content',
-                         'ffloor_net_radiation',
-                         'ffloor_sensible_heat',
-                         'ffloor_latent_heat',
-                         'ffloor_ground_heat',
-                         #'ffloor_energy_closure',
-                         #'ffloor_evaporation',
-                         #'ffloor_soil_evaporation',
-                         #'ffloor_throughfall',
-                         'ffloor_interception',
-                         #'ffloor_capillary_rise',
-                         #'ffloor_pond_recharge',
-                         #'ffloor_water_closure',
-                         'ffloor_net_co2',
-                         'ffloor_photosynthesis',
-                         'ffloor_respiration',
-                         'ffloor_soil_respiration',
-                         'ffloor_surface_temperature',
-                         #'ffloor_water_storage',
-                         #'ffloor_snow_water_equivalent',
-                         #'ffloor_par_albedo',
-                         #'ffloor_nir_albedo'],
-                         ], sim_idx=k)
-    )
-
-    import pickle
+    dd = []
+    m = 0
+    for d in data:        
+        print('converting: scen '  + str(k) + ' file ' + str(m))
+        nc = xarray_to_df(d, [
+                             'forcing_air_temperature',
+                             'forcing_precipitation',
+                             'forcing_pressure',
+                             'forcing_h2o',
+                             'forcing_co2',
+                             'forcing_wind_speed',
+                             'forcing_friction_velocity',
+                             'forcing_par',
+                             'forcing_nir',
+                             'forcing_lw_in',
+                             'canopy_LAI',
+                             #'canopy_energy_closure',
+                             #'canopy_fr_source',
+                             'canopy_SWnet',
+                             'canopy_LWnet',
+                             'canopy_Rnet',
+                             'canopy_interception',
+                             #'canopy_interception_storage',
+                             'canopy_evaporation',
+                             #'canopy_condensation',
+                             #'canopy_condensation_drip',
+                             'canopy_throughfall',
+                             #'canopy_water_closure',
+                             'canopy_SH',
+                             'canopy_LE',
+                             'canopy_NEE',
+                             'canopy_GPP',
+                             'canopy_Reco',
+                             'canopy_transpiration',
+                             #'pt_total_gpp',
+                             #'pt_total_dark_respiration',
+                             #'pt_total_transpiration',
+                             #'pt_total_stomatal_conductance_h2o',
+                             #'pt_total_boundary_conductance_h2o',
+                             #'soil_temperature',
+                             #'soil_volumetric_water_content',
+                             'ffloor_net_radiation',
+                             'ffloor_sensible_heat',
+                             'ffloor_latent_heat',
+                             'ffloor_ground_heat',
+                             #'ffloor_energy_closure',
+                             #'ffloor_evaporation',
+                             #'ffloor_soil_evaporation',
+                             #'ffloor_throughfall',
+                             'ffloor_interception',
+                             #'ffloor_capillary_rise',
+                             #'ffloor_pond_recharge',
+                             #'ffloor_water_closure',
+                             'ffloor_net_co2',
+                             'ffloor_photosynthesis',
+                             'ffloor_respiration',
+                             'ffloor_soil_respiration',
+                             'ffloor_surface_temperature',
+                             #'ffloor_water_storage',
+                             'ffloor_snow_water_equivalent',
+                             #'ffloor_par_albedo',
+                             #'ffloor_nir_albedo'],
+                             ], sim_idx=k)
+        dd.append(nc)
+        del nc
+        m += 1
+    df = pd.concat(dd)
+    out.append(df)
+    del df
+    
     ffile = r'results/Scenarios/S1/scens.pk'
     file = open(ffile, 'wb')
-    pickle.dump([dd, scens], file)
+    pickle.dump([out, scens], file)
     file.close()
     
---
+++ parameters/parametersets_S3.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 19 12:39:09 2018
+
+@author: ajkieloaho
+"""
+import numpy as np
+from parameters.parameter_tools import iterate_parameters
+from tools.iotools import read_forcing
+
+import itertools
+from copy import deepcopy as copy
+
+# Modifications to override default parameters in parameters.SmearII
+
+""" Simulation set 3: combinations """
+
+# default parameters for planttypes
+
+from parameters.generic_forest import gpara, cpara, spara
+
+default_params = {'general': gpara,
+                  'canopy': cpara,
+                  'soil': spara
+                 }
+        
+pts = cpara['planttypes']
+
+co2_mean = 385.0 #ppm
+
+rjv = pts['trees']['photop']['Jmax'] / pts['trees']['photop']['Vcmax']
+rrv = pts['trees']['photop']['Rd'] / pts['trees']['photop']['Vcmax']
+del pts
+
+# parameter ranges
+
+slai = ['LAI_r','LAI_l','LAI_h']
+sCO2 = ['CO2_r','CO2_l','CO2_h']
+sVcmax =['Vmax_r','Vmax_l','Vmax_h']
+sdec = ['df_r']
+
+lai = np.linspace(1.0, 7.0, 7)
+co2 = co2_mean * np.linspace(1.0, 1.6, 10)
+vcmax = [60.0] #, 80.0]
+m = [2.0, 4.0]
+
+# make all combinations
+#combnames = itertools.product(slai, sCO2, sVcmax, sdec)
+#combnames = list(combnames)
+comb = itertools.product(lai, co2, vcmax, m) #, Vcmax, g1)
+comb = list(comb)
+# number of combinations
+count = len(comb)
+print(count)
+
+# LAI per planttype
+LAI = tuple(comb[i][0] for i in range(len(comb)))
+CO2 = tuple(comb[i][1] for i in range(len(comb)))
+Vcmax = tuple(comb[i][2] for i in range(len(comb)))
+g1 = tuple(comb[i][3] for i in range(len(comb)))
+
+Jmax = tuple(rjv * comb[i][2] for i in range(len(comb)))
+Rd = tuple(rrv * comb[i][2] for i in range(len(comb)))
+
+def get_parameter_list_S3(year, listout=False):
+        
+    parameters = {
+        'count': count,
+        'scenario': 'S2',
+        'CO2': CO2,
+        
+        'canopy': {
+            'planttypes': {
+                'trees': {'LAImax': LAI,
+                         'photop': {
+                             'Vcmax': Vcmax,
+                             'Jmax': Jmax,
+                             'Rd': Rd}
+                         },
+                }
+            }
+        }
+    parameters['general'] = {
+                'start_time' : '%4d-07-01' %year,
+                'end_time' : '%4d-07-15' %year,
+                'forc_filename' : "Hyytiala/FIHy_forcing_2005-2010.dat"
+        }
+
+    # make parameter lists
+    param_list = [iterate_parameters(parameters, copy(default_params), c) for c in range(count)]
+    
+    # read forcing, replace CO2
+    start_time = param_list[0]['general']['start_time']
+    end_time = param_list[0]['general']['end_time']
+    dt = param_list[0]['general']['dt']
+    forc_filename = param_list[0]['general']['forc_filename']
+
+    
+    forcing = read_forcing(
+        forc_filename=forc_filename,
+        start_time=start_time,
+        end_time=end_time,
+        dt=dt
+    )
+    
+    for k in range(count):
+        #print(k)
+        forcing['CO2'] = param_list[k]['CO2']
+        param_list[k]['forcing'] = forcing.copy()
+        param_list[k]['nsim'] = k
+    if listout:
+        return param_list, comb
+    else:
+        return param_list
+
+
+# EOF
